3f5cb76e70a82baeea23a971fb92226364dd0ec9
Chaim <gchaimke@gmail.com>
2021-08-11 17:39:05 +0300

    dashboard update

--- inc/classes/WesDashboard.php
+++ inc/classes/WesDashboard.php
@@ -8,7 +8,7 @@ class WesDashboard
     {
         wp_add_dashboard_widget(
             'wporg_dashboard_widget',                          // Widget slug.
-            esc_html__('ERP Dashboard', 'wes'), // Title.
+            esc_html__('ERP Dashboard v.'.PLUGIN_NAME_VERSION , 'wes'), // Title.
             [$this, 'dashboar_content']                    // Display function.
         );
     }
@@ -17,11 +17,11 @@ class WesDashboard
     {
         $dir = ERP_DATA_FOLDER . "sync/";
         $files = glob($dir . "*.XML");
-        $html ="<h1>Last update</h1><div class='wes_dash_updates'>";
-        foreach ($files as $file) { // iterate files
-            $html .= "<div class='wes_row'><span>".basename($file) .  "</span>  <span>" .date("d-m-Y H:i:s", filemtime($file))."</span></div>";
+        $html = "<h1>Last update</h1><div class='wes_dash_updates'>";
+        foreach ($files as $file) {
+            $html .= "<div class='wes_row'><span>" . basename($file) .  "</span>  <span>" . date("d-m-Y H:i:s", filemtime($file)) . "</span></div>";
         }
-        echo $html."</div><a class='button' target='_blank' href='/wp-admin/admin.php?page=wesSettings&sync=true'>Sync Now</a>";
-        //_e("<h1>ERP last sync: " . date('d-m-y H:i') . "</h1>", "wes");
+        $html .= "</div><a class='button' target='_blank' href='/wp-admin/admin.php?page=wesSettings&sync=true'>Sync Now</a>";
+        echo $html;
     }
 }
